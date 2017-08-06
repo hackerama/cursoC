04c8fed8467014072c7366670cc07071d48fd2a2
hackerama <hackerama@protonmail.com>
2017-08-06 16:09:57 -0300

    update

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main(void) {
 	int ret = 0;
     printf("meu hello world em C\r\n");
-	printf("%x - %x - %x - %c\n", ret, 10, 0xa, 'A');
+	printf("%x - %x - %x - %x\n", ret, 10, 0xa, 'A');
     return 0;
 }
 
